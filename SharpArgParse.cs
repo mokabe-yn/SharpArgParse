1c20261e4a215830781c8a714b257832b6104756
M.Okabe(Home) <okabe_m@hmi.aitech.ac.jp>
2023-12-27 01:24:52 +0900

    メインファイル追加

---
+++ SharpArgParse.cs
@@ -0,0 +1,39 @@
+/* SharpArgParse
+ * Copyright (c) 2023 mokabe-yn <okabe_m@hmi.aitech.ac.jp>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. * 
+ * 
+ * */
+
+// Note: use only C#7.3 for .NET Framework 4.7.2 only environs.
+
+using System;
+using System.Collections.Generic;
+
+namespace SharpArgParse
+{
+    internal static class ArgParse
+    {
+        public static IEnumerable<string> Core(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
